48bb576edd9ca8fdec5b56d20118fa79616aa558
Nairobinivar12 <ingenieranairobi@gmail.com>
2021-10-05 13:31:55 +0200

    subiendo el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//RUTAS 
+import {ROUTES} from './app.routes'
+import {RouterModule  } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PdfComponent } from './components/pdf/pdf.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    PdfComponent,
+    ProyectosComponent,
+    ServiciosComponent,
+    TecnologiasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES,{useHash:true}), 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
